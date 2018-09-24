ce8a3041910be13aa2f471aa91c789fd6116d17a
Igor <segamanable@gmail.com>
2018-09-24 12:01:43 -0500

    Initial Commit

---
+++ LiveChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveChat
+//
+//  Created by Ihor Sihuta on 9/24/18.
+//  Copyright © 2018 hustlehard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
